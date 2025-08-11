514885c700116f1f5dd80ceaf5201ef53e906f23
Finlay Clark <finlay.clark@newcastle.ac.uk>
2025-08-11 16:17:38 +0100

    Select a given number of group ids in filter function

--- workflow/utils/filter.py
+++ workflow/utils/filter.py
@@ -2,6 +2,8 @@
 Filter the DES dataset to only contain records with elements of interest
 """
 
+from functools import partial
+
 import pandas as pd
 from loguru import logger
 from rdkit import Chem
@@ -35,9 +37,10 @@ def unwanted_molecules_filter(
 
 def filter_dataset(
     dataset_csv_path: str,
+    output_csv_path: str,
     elements_to_keep: list[str],
     ions_to_remove: list[str],
-    output_csv_path: str,
+    max_groups: int | None = None,
 ) -> None:
     """
     Filter the dataset to only contain records with elements of interest and no unwanted ions or charged molecules.
@@ -46,16 +49,23 @@ def filter_dataset(
     ----------
     dataset_csv_path : str
         Path to the input dataset CSV file.
+    output_csv_path : str
+        Path to save the filtered dataset CSV file.
     elements_to_keep : list[str]
         List of elements to keep in the dataset.
     ions_to_remove : list[str]
         List of ions to remove from the dataset.
-    output_csv_path : str
-        Path to save the filtered dataset CSV file.
+    max_groups : int | None
+        Maximum number of unique groups to keep in the dataset
+        (which specifies the chemical identities and type of data).
+        If None, all entries are returned. Entries are randomly selected
+        (with the random seed fixed.).
     """
     raw_dataset = pd.read_csv(dataset_csv_path)
     logger.info(
-        f"Loaded dataset with {len(raw_dataset)} records and {len(raw_dataset['system_id'].unique())} unique systems."
+        f"Loaded dataset with {len(raw_dataset)} records, "
+        f"{len(raw_dataset['system_id'].unique())} unique systems, "
+        f"and {len(raw_dataset['group_id'].unique())} unique groups."
     )
 
     unwanted_rows = raw_dataset.apply(
@@ -63,7 +73,28 @@ def filter_dataset(
     )
 
     filtered_dataset = raw_dataset[~unwanted_rows]
-    logger.info(f"Filtered dataset to {len(filtered_dataset)} records.")
+    logger.info(
+        f"Filtered dataset to {len(filtered_dataset)} records, "
+        f"{len(filtered_dataset['system_id'].unique())} unique systems, "
+        f"and {len(filtered_dataset['group_id'].unique())} unique groups."
+    )
+
+    if max_groups is not None:
+        groups = filtered_dataset["group_id"].unique()
+        selected_groups = np.random.choice(
+            groups, size=min(max_groups, len(groups)), replace=False
+        )
+        filtered_dataset = filtered_dataset[
+            filtered_dataset["group_id"].isin(selected_groups)
+        ]
+        logger.info(
+            f"Randomly selected {len(selected_groups)} groups from the filtered dataset."
+        )
+        logger.info(
+            f"Filtered dataset to {len(filtered_dataset)} records, "
+            f"{len(filtered_dataset['system_id'].unique())} unique systems, "
+            f"and {len(filtered_dataset['group_id'].unique())} unique groups."
+        )
 
     filtered_dataset.to_csv(output_csv_path, index=False)
 
@@ -77,12 +108,12 @@ filter_dimers_std = partial(
     filter_dataset,
     elements_to_keep=["H", "C", "N", "O", "F", "S", "Cl"],
     ions_to_remove=["[F-]", "[Cl-]", "[H][H]"],
-    max_entries=None,
+    max_groups=None,
 )
 
 filter_dimers_std_100 = partial(
     filter_dataset,
     elements_to_keep=["H", "C", "N", "O", "F", "S", "Cl"],
     ions_to_remove=["[F-]", "[Cl-]", "[H][H]"],
-    max_entries=100,
+    max_groups=100,
 )
